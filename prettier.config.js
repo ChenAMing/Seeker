c48f631cd4e2b213e209f40c0d7ed1ad9dcbdb03
ChenAMing <75087533+ChenAMing@users.noreply.github.com>
2023-06-13 22:42:23 +0800

    🎉 初始化

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    jsxBracketSameLine: true,
+    jsxSingleQuote: true,
+    printWidth: 100,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+}
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+export default defineConfig({
+    plugins: [vue({ script: { defineModel: true, propsDestructure: true } }), vueJsx()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+})
