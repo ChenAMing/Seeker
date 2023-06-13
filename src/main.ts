ff34aebecbd5fa3ee88b2125bddefc62a75e9015
ChenAMing <75087533+ChenAMing@users.noreply.github.com>
2023-06-13 23:03:27 +0800

    ✏️ 配置 tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import './styles/app.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import modules from './modules'
 
-createApp(App).mount('#app')
+createApp(App).use(modules).mount('#app')
