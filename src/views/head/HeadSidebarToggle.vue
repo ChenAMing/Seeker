a24a8a858e5c129609ddc10fd654300b947b8c14
ChenAMing <75087533+ChenAMing@users.noreply.github.com>
2023-06-14 23:46:54 +0800

    ✨ 添加侧边栏打开按钮隐藏时的装饰

--- src/views/head/HeadSidebarToggle.vue
+++ src/views/head/HeadSidebarToggle.vue
@@ -5,5 +5,11 @@ const side = useSide()
 </script>
 
 <template>
-    <IconButton icon="icon-[solar--hamburger-menu-line-duotone]" @click="side.toggle" class="z-30" />
+    <IconButton
+        icon="icon-[solar--hamburger-menu-line-duotone]"
+        @click="side.toggle"
+        class="z-30 md:pointer-events-none md:hidden" />
+    <span class="pointer-events-none inline-block h-10 w-10 p-2.5 max-md:hidden">
+        <span class="icon-[solar--map-arrow-right-bold-duotone] !h-5 !w-5 text-pri"></span>
+    </span>
 </template>
