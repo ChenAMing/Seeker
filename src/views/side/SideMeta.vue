15ca73c03ec5f6602412bc3b1cea33d6f8aa085e
ChenAMing <75087533+ChenAMing@users.noreply.github.com>
2023-06-15 15:58:20 +0800

    ⬆️ 使用组件替换原有逻辑、侧边栏布局调整

--- src/views/side/SideMeta.vue
+++ src/views/side/SideMeta.vue
@@ -1,40 +1,24 @@
 <script lang="ts" setup>
 import { useMeta, useSide } from '@/stores'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 
 const route = useRoute()
-const router = useRouter()
 const meta = useMeta()
 const side = useSide()
-
-function go(id: string) {
-    router.push({ name: 'list', query: { id } })
-    if (side.show) side.show = false
-}
 </script>
 
 <template>
-    <div class="mb-1 px-3 text-xs">全部清单</div>
-
-    <div class="flex flex-col gap-1 overflow-y-auto p-1">
-        <button
-            type="button"
-            v-for="item in meta.all"
-            :key="item.name"
-            @click="go(item.id)"
-            class="flex min-h-[2rem] items-center gap-6 rounded px-3 transition-colors duration-300 hover:bg-mask active:bg-mask-more"
-            :class="{ 'pointer-events-none bg-sec hover:bg-sec': route.query.id === item.id }">
-            <span
-                class="pointer-events-none !h-4 min-w-[1rem] transition-colors duration-150"
-                :class="[
-                    { 'text-on-sec': route.query.id === item.id },
-                    'icon-[solar--notes-minimalistic-bold-duotone]',
-                ]"></span>
-            <span
-                class="pointer-events-none truncate transition-colors duration-150"
-                :class="{ 'text-on-sec': route.query.id === item.id }">
+    <TheCollapse icon="icon-[solar--widget-bold-duotone]" title="全部清单">
+        <Navigation @after-go="side.show = false">
+            <NavigationItem
+                type="secondary"
+                icon="icon-[solar--notes-minimalistic-bold-duotone]"
+                v-for="item in meta.all"
+                :key="item.id"
+                :selected="route.query.id === item.id"
+                :route-location="{ name: 'list', query: { id: item.id } }">
                 {{ item.name }}
-            </span>
-        </button>
-    </div>
+            </NavigationItem>
+        </Navigation>
+    </TheCollapse>
 </template>
--- src/views/side/index.tsx
+++ src/views/side/index.tsx
@@ -3,6 +3,7 @@ import { useSide } from '@/stores'
 import { useTransition } from '@/composables'
 import SideLink from './SideLink.vue'
 import SideLogo from './SideLogo.vue'
+import SideStar from './SideStar.vue'
 import SideMeta from './SideMeta.vue'
 
 export default {
@@ -20,12 +21,13 @@ export default {
 
                 <div
                     class={[
-                        'fixed inset-y-0 left-0 flex h-100vh w-60 flex-col gap-1 bg-sur transition duration-300 max-md:z-20',
+                        'fixed inset-y-0 left-0 flex h-100vh w-60 flex-col gap-1 overflow-y-hidden bg-sur transition duration-300 hover:overflow-y-auto max-md:z-20',
                         side.show ? 'max-md:translate-x-0' : 'max-md:-translate-x-full',
                     ]}>
                     {/* Include */}
                     <SideLogo />
                     <SideLink />
+                    <SideStar />
                     <SideMeta />
                 </div>
             </div>
